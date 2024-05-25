3834f63e0a00e42f323143fa1c136869a980f4e8
Flavio Milani <frmilanij@gmail.com>
2024-05-25 16:32:08 -0300

    All examples terminal e argumentos chapter

---
+++ src/terminaleargumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.flaviomxx.terminaleargumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, estou sendo executado pelo terminal!");
+    }
+}
